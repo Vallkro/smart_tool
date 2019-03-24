d74d206261ec80afb6584e7168b0f519a6e3675c
Valdemar Krona <kronav@student.chalmers.se>
2019-03-24 21:29:15 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'smart_tool'
+
+setup(
+    name=package_name,
+    version='0.6.2',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Esteve Fernandez',
+    author_email='esteve@osrfoundation.org',
+    maintainer='Mikael Arguedas',
+    maintainer_email='mikael@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'Python nodes which were previously in the ros2/examples repository '
+        'but are now just used for demo purposes.'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'listener = smart_tool.topics.listener:main',
+            'talker = smart_tool.topics.talker:main',
+            'listener_qos = smart_tool.topics.listener_qos:main',
+            'talker_qos = smart_tool.topics.talker_qos:main',
+            'listener_serialized = smart_tool.topics.listener_serialized:main',
+            'add_two_ints_client = smart_tool.services.add_two_ints_client:main',
+            'add_two_ints_client_async = smart_tool.services.add_two_ints_client_async:main',
+            'add_two_ints_server = smart_tool.services.add_two_ints_server:main'
+        ],
+    },
+)
